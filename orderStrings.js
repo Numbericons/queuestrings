e3f50eb9241657faf909bc2ceff56c8ecfd758ba
Zachary <zachjoliver@gmail.com>
2019-11-07 23:30:52 -0800

    Works but not exact on the sequence of items

--- orderStrings.js
+++ orderStrings.js
@@ -5,12 +5,19 @@ function getOrder(input) {
   let retStr = "";
   let strInput = input.slice(0);
   while (strInput.length) {
-    valid.forEach((item) => {
-      let idx = strInput.index(item);
-    })
+    for(let i=0; i < valid.length; i++) {
+      let idx = strInput.indexOf(valid[i]);
+      while (idx > -1) {
+        let newStr = strInput.slice(0, idx) + strInput.slice(idx + valid[i].length, strInput.length);
+        strInput = newStr;
+        retStr += valid[i][0].toUpperCase() + valid[i].slice(1) + " ";
+        idx = strInput.indexOf(valid[i]);
+      }
+    }
   }
 
-  return retStr; 
+  return retStr.slice(0,retStr.length - 1); 
 }
 
-getOrder('milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza');
+const x = getOrder('milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza');
+console.log(x);
